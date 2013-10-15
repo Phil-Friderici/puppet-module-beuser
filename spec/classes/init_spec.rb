96b332416d2ee771079d13475e6d9cce3d23b9e4
Philipp van Huellen <Philipp.van.Huellen@ericsson.com>
2013-10-15 10:27:28 +0200

    Adapted new default $mode in spec-test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -19,7 +19,7 @@ describe 'beuser' do
         'path'   => '/bin/beuser',
         'owner'  => 'root',
         'group'  => 'root',
-        'mode'   => '4755',
+        'mode'   => '0755',
       } )
     }
   end
@@ -41,7 +41,7 @@ describe 'beuser' do
         'path'   => '/bin/beuser',
         'owner'  => 'root',
         'group'  => 'root',
-        'mode'   => '4755',
+        'mode'   => '0755',
       } )
     }
   end
@@ -64,7 +64,7 @@ describe 'beuser' do
         'path'   => '/bin/beuser',
         'owner'  => 'root',
         'group'  => 'root',
-        'mode'   => '4755',
+        'mode'   => '0755',
       } )
     }
   end
@@ -86,7 +86,7 @@ describe 'beuser' do
         'path'   => '/bin/beuser',
         'owner'  => 'root',
         'group'  => 'root',
-        'mode'   => '4755',
+        'mode'   => '0755',
       } )
     }
   end
@@ -108,7 +108,7 @@ describe 'beuser' do
         'path'   => '/bin/beuser',
         'owner'  => 'root',
         'group'  => 'root',
-        'mode'   => '4755',
+        'mode'   => '0755',
       } )
     }
   end
