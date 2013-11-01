2337d44aa71d35fbb037e32ffe3eb46174fa63b1
Johan Wennerberg <johan.x.wennerberg@ericsson.com>
2013-11-01 14:19:41 +0100

    Add support for package provider and source attributes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,31 @@
 class beuser (
   $ensure         = 'present',
   $package_name   = 'beuser',
+  $adminfile      = undef,
+  $provider       = undef,
+  $source         = undef,
 ) {
 
   package { 'beuser':
-    ensure  => $ensure,
-    name    => $package_name,
+    ensure => $ensure,
+    name   => $package_name,
+  }
+
+  if $provider != undef {
+    Package['beuser'] {
+      provider => $provider,
+    }
+  }
+
+  if $source != undef {
+    Package['beuser'] {
+      source => $source,
+    }
+  }
+
+  if $adminfile != undef {
+    Package['beuser'] {
+      adminfile => $adminfile,
+    }
   }
 }
